9153a43a547d7fd53d1de77913c25625b9b7f6f1
MCHowl <_mattc@hotmail.com>
2017-12-08 21:10:18 +0800

    Added Main Menu

---
+++ Assets/Script/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	private GameObject menuCanvas;
+	private GameObject instructionsCanvas;
+	private GameObject creditsCanvas;
+
+	void Start () {
+		menuCanvas = GameObject.Find ("Main Canvas");
+		instructionsCanvas = GameObject.Find ("Instructions Canvas");
+		creditsCanvas = GameObject.Find ("Credits Canvas");
+
+		DisableCanvas (instructionsCanvas);
+		DisableCanvas (creditsCanvas);
+	}
+	
+	public void DisableCanvas(GameObject canvas) {
+		canvas.SetActive(false);
+	}
+
+	public void EnableCanvas(GameObject canvas) {
+		canvas.SetActive(true);
+	}
+
+	public void LoadMain() {
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+
+
+}
